83571e56aa6125931d80dcc0017bbc5f198dbd17
Joel Borggrén-Franck <jbf@codehouse.se>
2009-03-14 18:45:39 +0100

    Added util.h with some macros.
    Signed-off-by: Joel Borggrén-Franck <jbf@codehouse.se>

---
+++ util.h
@@ -0,0 +1,20 @@
+#ifndef __UTIL_H
+#define __UTIL_H 1
+
+#include <stdlib.h>
+
+#define new(type) ((type *)malloc(sizeof(type)/sizeof(char)))
+
+#define min(x,y) ({ \
+      typeof(x) _x = (x);                       \
+      typeof(y) _y = (y);                       \
+      (void) (&_x == &_y);                      \
+      _x < _y ? _x : _y; })
+
+#define max(x,y) ({ \
+      typeof(x) _x = (x);                       \
+      typeof(y) _y = (y);                       \
+      (void) (&_x == &_y);                      \
+      _x > _y ? _x : _y; })
+
+#endif /* __UTIL_H */
