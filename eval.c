8c4e40b31ee64c0985b6f433f6f0ec22d2fadfa1
Joel Borggrén-Franck <jbf@codehouse.se>
2009-08-13 23:30:49 +0200

    Initial add of eval.

---
+++ eval.c
@@ -0,0 +1,22 @@
+#include "cell.h"
+#include "symbol.h"
+#include "reader.h"
+#include "util.h"
+
+cell_t *evaluate(cell_t *exp /* later add env */) {
+  if (NULL == exp) {
+    return NULL;
+  } else if (ATOMP(exp)) {
+    /*if (SYMBOLP(exp)) {
+      return lookup(exp->symbol, env);
+      } else */ if (STRINGP(exp) || NUMBERP(exp)) {
+      return exp;
+    } else {
+      return NULL; /* error */
+    }
+  } else if (PAIRP(exp)) {
+    return NULL; /* evaluate car, invoke on cdr */
+  } else {
+    return NULL;
+  }
+}
---
+++ eval.h
@@ -0,0 +1,4 @@
+#ifndef _EVAL_H
+#define _EVAL_H 1
+
+#endif /* _EVAL_H */
