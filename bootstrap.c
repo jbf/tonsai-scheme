c79b6c9d0474fe58c931fcf325c291bfd7f509ee
Joel Borggrén-Franck <jbf@codehouse.se>
2010-01-09 00:56:34 +0100

    Added 't'-cell/sym for convenience.
    Signed-off-by: Joel Borggrén-Franck <jbf@codehouse.se>

--- bootstrap.c
+++ bootstrap.c
@@ -8,15 +8,19 @@
 
 static cell_t nil_c;
 static cell_t false_c;
+static cell_t t_c;
 
 static symbol_entry_t nil_s = {(unsigned char *)"NIL", &nil_c};
 static symbol_entry_t false_s = {(unsigned char *)"FALSE", &false_c};
+static symbol_entry_t t_s = {(unsigned char *)"t", &t_c};
 
 static cell_t nil_c = {{.type = PAYLOAD_NIL}, {.symbol = &nil_s}};
 static cell_t false_c = {{.type = PAYLOAD_SYMBOL}, {.symbol = &false_s}};
+static cell_t t_c = {{.type = PAYLOAD_SYMBOL}, {.symbol = &t_s}};
 
 cell_t *nil_cell = &nil_c;
 cell_t *false_cell = &false_c;
+cell_t *t_cell = &t_c;
 
 /*
  * Creates an initial environment containing nil. Sets '**env' to NULL in
@@ -34,6 +38,7 @@ int fill_initial_environment(environ_t *env) {
   /* Add lots of stuff */
   add_to_environment(env, nil_cell, nil_cell);
   add_to_environment(env, false_cell, false_cell);
+  add_to_environment(env, t_cell, t_cell);
 
   return ENV_CREATED_OK;
 }
@@ -47,6 +52,7 @@ int boot(symbol_table *tab, environ_t **env) {
   init_symbol_table(tab);
   push(tab, &nil_s);
   push(tab, &false_s);
+  push(tab, &t_s);
 
   create_empty_environment(env);
   fill_initial_environment(*env);
