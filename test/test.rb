3acb8ca6b8521b1532325fca2bdd0d30ee14e736
Joel Borggrén-Franck <jbf@codehouse.se>
2010-03-04 00:13:31 +0100

    Simple test framework, and some sanity tests.

---
+++ test/test.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require 'stringio'
+
+f = File.new("sanity.test")
+
+failed = false
+f.each do | line |
+  m = /(.*),(.*)/.match(line)
+  cmd = "echo \'#{m[1]}\' | ../repl 2>/dev/null"
+  want = m[2].strip
+  ret = `#{cmd}`
+  l = StringIO.new(ret).readlines.last.chomp
+  puts "Test failed: #{m[1]} #{cmd} #{l} #{want}" unless (want == l)
+  failed = true unless (want == l)
+end
+
+puts "All tests pass" unless failed
