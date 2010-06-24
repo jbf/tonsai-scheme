030f378d242f762d9e08bd3e27501468f7a8b98d
Joel Borggrén-Franck <joel.franck@oracle.com>
2010-06-24 09:54:52 +0200

    Fixed liveframes ;)

--- liveness.c
+++ liveness.c
@@ -3,11 +3,12 @@
 
 #include "util.h"
 #include "memory.h"
+#include "cell.h"
 
 typedef struct frame_t {
   struct frame_t *next_frame;
   int nr_live;
-  struct cell_t **live_cells; /* this is an array of 'nr_live' (cell_t *)s */
+  cell_t **live_cells; /* this is an array of 'nr_live' (cell_t *)s */
 } frame_t;
 
 #define _LIVENESS_C 1
@@ -16,26 +17,31 @@ typedef struct frame_t {
 
 frame_t *live_root;
 
+void print_single_frame(frame_t *frame);
+
 frame_t *new_liveframe(int nr_live, ...) {
   va_list ap;
-  struct cell_t **list;
+  cell_t **list, **arr;
   frame_t *frame;
+  int i = 0;
 
   if (nr_live < 1) {
     DEBUGPRINT_("Trying to make a frame_t with < 1 live cell_t:s. Aborting.\n");
     bail(1);
   } 
 
-  list = (struct cell_t **)malloc_or_bail(sizeof(struct cell_t *)*nr_live/sizeof(char));
+  list = (cell_t **)malloc_or_bail(sizeof(cell_t *)*nr_live/sizeof(char));
   frame = (frame_t *)malloc_or_bail(sizeof(frame_t)/sizeof(char));
   
   frame->nr_live = nr_live;
   frame->live_cells = list;
+  arr = list;
 
   va_start(ap, nr_live);
-  for (; nr_live > 0; nr_live--) {
-    list = va_arg(ap, void *);
-    list++;
+  for (i = 0; i < nr_live; i++) {
+     cell_t *foo = va_arg(ap, cell_t *);
+     *arr = foo;
+     arr++;
   }
   va_end(ap);
 
@@ -59,4 +65,24 @@ void dealloc_frames(frame_t **top) {
   while (*top != NULL) {
     pop_liveness(top);
   }
+}
+
+void print_frames(frame_t *first) {
+   frame_t *f = first;
+   while(f != NULL) {
+      print_single_frame(f);
+      f=f->next_frame;
+   }
+}
+
+void print_single_frame(frame_t *f) {
+   cell_t **cp = f->live_cells;
+   int i = 0;
+   while (i < f->nr_live) {
+      DEBUGPRINT("Live cell_t at: %p ", *cp);
+      pp(*cp);
+      printf("\n");
+      i++;
+      cp++;
+   }
 }
--- liveness.h
+++ liveness.h
@@ -12,4 +12,6 @@ void push_liveness(frame_t **top, frame_t *new_top);
 void pop_liveness(frame_t **top);
 void dealloc_frames(frame_t **top);
 
+void print_frames(frame_t *first);
+
 #endif /* _LIVENESS_H */
